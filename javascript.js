f092da01c43fa238214158bfd9c84b33ec008758
1sirm <1siraj.1merie.1@gmail.com>
2024-09-18 20:25:54 +0300

    [COMMITED] changes

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Change Html Content
+function changehtml() { document.getElementById('changehtml').innerHTML = 'Javascript changed this text'; }
+
+
+// Change src
+function changesrctosquare() { document.getElementById('changephotosrc').src = '/imgs/square.png' }
+function changesrctocircle() { document.getElementById('changephotosrc').src = '/imgs/circle.png' }
+
+
+// Change Style
+function changestyle() { document.getElementById('changestyle').style.fontSize = '35px' }
+
+
